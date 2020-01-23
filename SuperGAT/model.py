9a238a9e11f0bd5743d8ec7f3d3a8b36fd163ad7
Dongkwan Kim <todoaskit@gmail.com>
2020-01-23 13:33:37 +0900

    Add LargeSuperGATNet

--- SuperGAT/model.py
+++ SuperGAT/model.py
@@ -14,7 +14,7 @@ from typing import Tuple, List
 
 
 def _get_gat_cls(attention_name: str):
-    if attention_name == "GAT" or attention_name == "GATPPI":
+    if attention_name in ["GAT", "GATPPI", "LargeGAT"]:
         return SuperGAT
     else:
         raise ValueError("{} is not proper name".format(attention_name))
@@ -43,13 +43,6 @@ def _inspect_attention_tensor(x, edge_index, att_res) -> bool:
         return True
     else:
         return False
-
-
-def to_pool_cls(pool_name):
-    if pool_name in pygnn.glob.__all__ or pool_name in pygnn.pool.__all__:
-        return eval("tgnn.{}".format(pool_name))
-    else:
-        raise ValueError("{} is not in {} or {}".format(pool_name, pygnn.glob.__all__, pygnn.pool.__all__))
 
 
 class SuperGATNet(nn.Module):
@@ -171,3 +164,67 @@ class SuperGATNetPPI(nn.Module):
         #     _inspect_attention_tensor(x, edge_index, self.conv2.residuals)
 
         return x
+
+
+class LargeSuperGATNet(nn.Module):
+
+    def __init__(self, args, dataset_or_loader):
+        super().__init__()
+        self.args = args
+        self.num_layers = self.args.num_layers
+
+        gat_cls = _get_gat_cls(self.args.model_name)
+
+        num_input_features = getattr_d(dataset_or_loader, "num_node_features")
+        num_classes = getattr_d(dataset_or_loader, "num_classes")
+
+        conv_common_kwargs = dict(
+            dropout=args.dropout,
+            is_super_gat=args.is_super_gat,
+            attention_type=args.attention_type,
+            super_gat_criterion=args.super_gat_criterion,
+            neg_sample_ratio=args.neg_sample_ratio,
+            pretraining_noise_ratio=args.pretraining_noise_ratio,
+            use_pretraining=args.use_pretraining,
+        )
+        self.conv_list = []
+        for conv_id in range(1, self.num_layers + 1):
+            if conv_id == 1:  # first layer
+                in_channels, out_channels = num_input_features, args.num_hidden_features
+                heads, concat = args.heads, True
+            elif conv_id == self.num_layers:  # last layer
+                in_channels, out_channels = args.num_hidden_features * args.heads, num_classes
+                heads, concat = args.out_heads or args.heads, False
+            else:
+                in_channels, out_channels = args.num_hidden_features * args.heads, args.num_hidden_features
+                heads, concat = args.heads, True
+            conv = gat_cls(in_channels, out_channels, heads=heads, concat=concat, **conv_common_kwargs)
+            conv_name = "conv{}".format(conv_id)
+            self.conv_list.append(conv)
+            setattr(self, conv_name, conv)
+            self.add_module(conv_name, conv)
+
+        pprint(next(self.modules()))
+
+    def forward(self, x, edge_index, batch=None, **kwargs) -> torch.Tensor:
+        for conv_idx, conv in enumerate(self.conv_list):
+            x = F.dropout(x, p=self.args.dropout, training=self.training)
+            x = conv(x, edge_index, **kwargs)
+            if conv_idx != self.num_layers - 1:
+                x = F.elu(x)
+        return x
+
+    def set_layer_attrs(self, name, value):
+        for conv in self.conv_list:
+            setattr(conv, name, value)
+
+    def get_attention_dist_by_layer(self, edge_index, num_nodes) -> List[List[torch.Tensor]]:
+        """
+        :param edge_index: tensor the shape of which is [2, E]
+        :param num_nodes: number of nodes
+        :return List[List[torch.Tensor]]: [L, N, [#neighbors, heads]]
+        """
+        attention_dist_by_layer = []
+        for conv in self.conv_list:
+            attention_dist_by_layer.append(conv.get_attention_dist(edge_index, num_nodes))
+        return attention_dist_by_layer
